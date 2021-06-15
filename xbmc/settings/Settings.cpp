f0b23bf9fbf7ee3c3306814ed2c47fcc39e97ff8
Maven85 <janis.semper@googlemail.com>
2021-06-15 09:00:23 +0200

    [Android] picture-in-picture

--- xbmc/settings/Settings.cpp
+++ xbmc/settings/Settings.cpp
@@ -153,6 +153,7 @@ constexpr const char* CSettings::SETTING_VIDEOPLAYER_USEPRIMEDECODER;
 constexpr const char* CSettings::SETTING_VIDEOPLAYER_USESTAGEFRIGHT;
 constexpr const char* CSettings::SETTING_VIDEOPLAYER_LIMITGUIUPDATE;
 constexpr const char* CSettings::SETTING_VIDEOPLAYER_SUPPORTMVC;
+constexpr const char* CSettings::SETTING_VIDEOPLAYER_USEPIP;
 constexpr const char* CSettings::SETTING_MYVIDEOS_SELECTACTION;
 constexpr const char* CSettings::SETTING_MYVIDEOS_USETAGS;
 constexpr const char* CSettings::SETTING_MYVIDEOS_EXTRACTFLAGS;
